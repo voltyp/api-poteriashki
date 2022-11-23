f2e05600755cf6563452800a70fc4c5eb4ab303b
yuri.voltermakher <yuri.voltermakher@itgarden.ru>
2022-11-23 20:36:41 +0300

    add migration

---
+++ src/database/migrations/1669184706079-UserTable.ts
@@ -0,0 +1,22 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class UserTable1669184706079 implements MigrationInterface {
+    name = 'UserTable1669184706079'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TYPE "public"."users_role_enum" AS ENUM('0', '1', '2')`);
+        await queryRunner.query(`CREATE TYPE "public"."users_status_enum" AS ENUM('0', '1')`);
+        await queryRunner.query(`CREATE TABLE "users" ("id" SERIAL NOT NULL, "firstName" character varying NOT NULL, "lastName" character varying NOT NULL, "middleName" character varying, "email" character varying NOT NULL, "phone" character varying, "password" bytea, "birthDate" TIMESTAMP, "lastLogin" TIMESTAMP WITH TIME ZONE, "hashedRt" bytea, "role" "public"."users_role_enum" NOT NULL DEFAULT '2', "status" "public"."users_status_enum" NOT NULL DEFAULT '0', "lastChangePasswordRequest" TIMESTAMP, "hashedRequest" character varying, CONSTRAINT "UQ_97672ac88f789774dd47f7c8be3" UNIQUE ("email"), CONSTRAINT "UQ_a000cca60bcf04454e727699490" UNIQUE ("phone"), CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id"))`);
+        await queryRunner.query(`CREATE INDEX "IDX_5372672fbfd1677205e0ce3ece" ON "users" ("firstName") `);
+        await queryRunner.query(`CREATE INDEX "IDX_af99afb7cf88ce20aff6977e68" ON "users" ("lastName") `);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP INDEX "public"."IDX_af99afb7cf88ce20aff6977e68"`);
+        await queryRunner.query(`DROP INDEX "public"."IDX_5372672fbfd1677205e0ce3ece"`);
+        await queryRunner.query(`DROP TABLE "users"`);
+        await queryRunner.query(`DROP TYPE "public"."users_status_enum"`);
+        await queryRunner.query(`DROP TYPE "public"."users_role_enum"`);
+    }
+
+}
