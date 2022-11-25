f2146197efdeecf1815ca0ea804c6c274e515245
Sergey Kravchenko <kravchenko.sgv@gmail.com>
2022-11-25 15:23:23 +0300

    feat: exclude fields from user entity

--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -7,6 +7,8 @@ import {
   Delete,
   ParseIntPipe,
   Get,
+  ClassSerializerInterceptor,
+  UseInterceptors,
 } from '@nestjs/common';
 
 import { UserService } from './user.service';
@@ -17,21 +19,25 @@ import { UserUpdateDto } from './dto/user-update.dto';
 export class UserController {
   constructor(readonly userService: UserService) {}
 
+  @UseInterceptors(ClassSerializerInterceptor)
   @Get(':id')
   async getUser(@Param('id', ParseIntPipe) id: number) {
     return this.userService.getUser(id);
   }
 
+  @UseInterceptors(ClassSerializerInterceptor)
   @Get()
   async getAllUsers() {
     return this.userService.getAllUsers();
   }
 
+  @UseInterceptors(ClassSerializerInterceptor)
   @Post()
   async createUser(@Body() data: UserCreateDto) {
     return this.userService.createUser(data);
   }
 
+  @UseInterceptors(ClassSerializerInterceptor)
   @Put(':id')
   async updateUser(
     @Param('id', ParseIntPipe) id: number,
