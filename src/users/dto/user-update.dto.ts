5011c42642255eeaa8667ce5c0d500b44e0f5209
Sergey Kravchenko <kravchenko.sgv@gmail.com>
2022-11-25 11:29:01 +0300

    feat: add new fields in user update dto

--- src/users/dto/user-update.dto.ts
+++ src/users/dto/user-update.dto.ts
@@ -2,6 +2,10 @@ import { IsNotEmpty } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class UserUpdateDto {
+  @ApiProperty({ example: 1 })
+  @IsNotEmpty()
+  readonly id: number;
+
   @ApiProperty({ example: 'Иван' })
   @IsNotEmpty()
   readonly firstName: string;
@@ -10,7 +14,19 @@ export class UserUpdateDto {
   @IsNotEmpty()
   readonly lastName: string;
 
+  @ApiProperty({ example: 'Викторович' })
+  @IsNotEmpty()
+  readonly middleName: string;
+
   @ApiProperty({ example: 'test1@example.com' })
   @IsNotEmpty()
   readonly email: string;
+
+  @ApiProperty({ example: '7-999-999-99-99' })
+  @IsNotEmpty()
+  readonly phone: string;
+
+  @ApiProperty({ example: '25.11.2022' })
+  @IsNotEmpty()
+  readonly birthDate: Date;
 }
